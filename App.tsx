d277708142e5af37259b39573f9181acbbc8e2a2
Mohamed Abu Basith <mohamedabu.basith@kobil.com>
2025-02-16 00:50:39 +0530

    Scren changes

--- App.tsx
+++ App.tsx
@@ -2,7 +2,9 @@ import { Provider } from 'react-redux'
 import { store } from './core/state_management/store';
 import { StackNagivation } from './router/StackNavigation';
 import { NotificationService } from './services/NotificationService';
+import { enableScreens } from 'react-native-screens';
 
+enableScreens(false);
 
 export default function App() {
   return (
