18e03066055043164e62b32cc6eb30d91e835720
Mohamed Abu Basith <mohamedabu.basith@kobil.com>
2025-02-16 01:08:46 +0530

    App config changes.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
-import { Provider } from 'react-redux'
+import { useEffect } from 'react';
+import { enableScreens } from 'react-native-screens';
+import { Provider } from 'react-redux';
 import { store } from './core/state_management/store';
 import { StackNagivation } from './router/StackNavigation';
 import { NotificationService } from './services/NotificationService';
-import { enableScreens } from 'react-native-screens';
-
-enableScreens(false);
+import { View } from 'react-native';
 
 export default function App() {
+  useEffect(() => {
+    enableScreens(false); // Ensure it takes effect after the app loads
+  }, []);
+
   return (
     <Provider store={store}>
       <NotificationService />
-      <StackNagivation />
+      <View style={{ flex: 1 }}> 
+        <StackNagivation />
+      </View>
     </Provider>
   );
 }
